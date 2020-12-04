08656e07f886368a5ba4a133389c21c27210f4c6
wh1t3fox <dev@exploit.design>
2020-12-04 11:27:06 -0500

    modify script

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
         "PyYAML==5.3.1",
     ],
     extras_require={
-        "dev": ["black==20.8b1", "twine==3.2.0"],
+        "dev": ["black==20.8b1", "twine==3.2.0", "pytest==6.1.2"],
     },
     entry_points={
         "console_scripts": [
