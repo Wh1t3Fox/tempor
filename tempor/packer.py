f5e3961cbed93941e57d32e7416871d2d9ce1228
wh1t3fox <dev@exploit.design>
2025-04-25 06:14:46 -0400

    :art: print image id after creation

--- tempor/packer.py
+++ tempor/packer.py
@@ -196,6 +196,9 @@ class Packer:
             case _:
                 self.packer_img_id = stdout.split('\n')[-3].split(':')[-1].strip()
 
+        if self.packer_img_id:
+            self.logger.info('[green]Created image {self.packer_img_id}[/]')
+
     def fmt(self):
         """Packer fmt.
 
