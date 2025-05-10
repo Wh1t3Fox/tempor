82aa6b7acfc8ad04a2e817ee5ad1e7de00dcbf12
wh1t3fox <dev@exploit.design>
2025-05-10 12:09:11 -0400

    :sparkles: digitalocean ipv6 support

--- tempor/providers/digitalocean/main.tf
+++ tempor/providers/digitalocean/main.tf
@@ -7,14 +7,6 @@ resource "digitalocean_ssh_key" "default" {
     public_key = file("${path.module}/files/${var.region}/${var.image}/${var.vps_name == "" ? data.external.vps_name.result.name : var.vps_name}/.ssh/id_ed25519.pub")
 }
 
-#data "template_file" "script" {
-#    template = file("${path.module}/files/cloud-init.yml")
-#    vars = {
-#        username = var.username,
-#        public_key = digitalocean_ssh_key.default.public_key
-#    }
-#}
-
 resource "digitalocean_droplet" "vps" {
     count = var.num
     image = var.image
@@ -24,5 +16,5 @@ resource "digitalocean_droplet" "vps" {
     ssh_keys = [
         digitalocean_ssh_key.default.fingerprint
     ]
-    # user_data = data.template_file.script.rendered
+    ipv6 = true
 }
